0b76c7d5c911e29e1412fb67c5a9b8f882716a92
Hospuntake <91428070+Hospuntake@users.noreply.github.com>
2021-09-26 20:36:04 +0200

    Create 0labdef.c

---
+++ 0labdef.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+int main(){
+    int fi=0;
+    while(fi==0){
+        printf("Indiqui el primer nombre\n->");
+        char caracter[100];
+        scanf("%s",caracter);
+        printf("Indiqui el segon nombre\n->");
+        char caracter2[100];
+        scanf("%s",caracter2);
+        int a=0;
+        int n=0;
+        int comoEntero = caracter[n] - '0';   
+        int resp=0;
+        if(comoEntero==-38){
+            printf("Introduzca un carácter porfavor\n");
+        }
+        else{
+            while(a==0 ){
+                //printf("%d\n",n);
+                if((!(comoEntero>=0 && comoEntero<=9))){
+                    if(comoEntero==-48){
+                        a=1;
+                        resp=1;   
+                    }
+                    else if(comoEntero==-3 && a==0){
+                    }
+                    else{
+                        a=1;
+                    } 
+                }
+                ++n;
+                comoEntero = caracter[n] - '0';
+
+            }
+            
+        }
+        int a2=0;
+        int n2=0;
+        int comoEntero2 = caracter2[n2] - '0';   
+        int resp2=0;
+        if(comoEntero2==-38){
+            printf("Introduzca un carácter porfavor\n");
+        }
+        else{
+            while(a2==0 ){
+                if((!(comoEntero2>=0 && comoEntero2<=9))){
+                    if(comoEntero2==-48){
+                        a2=1;
+                        resp2=1;   
+                    }
+                    else if(comoEntero2==-3 && a2==0){
+                    }
+                    else{
+                        a2=1;
+                    } 
+                }
+                ++n2;
+                comoEntero2 = caracter2[n2] - '0';
+
+            }
+            
+        }
+
+
+
+        if(resp==0){
+            printf("El primer nombre no és enter\n");
+
+        }
+        if(resp==1){
+            printf("El primer nombre és enter\n");
+
+        }
+        if(resp2==0){
+            printf("El segón nombre no és enter\n");
+
+        }
+        if(resp2==1){
+            printf("El segón nombre és enter\n");
+
+        }
+        int correcte=0;
+          
+        while(correcte==0){
+            printf("¿Vol continuar inserint numeros?\n");
+            printf("Escrigui 1 per continuar\nEscrigui 2 per acabar \n\n->");
+            char a[100];
+            scanf("%s",a);
+            int comoEntero4 = a[0] - '0'; 
+            if(comoEntero4=='1'&& a[1]=='\0'){
+                correcte=1;
+            }
+            else if(a[0]=='2'&& a[1]=='\0'){
+                fi=1;
+                correcte=1;
+            }
+            else {
+                printf("Numero incorrecte\n");
+                
+            }
+        }
+    }
+}
