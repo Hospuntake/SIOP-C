49c15f7fe78d9b0071c86997b980816ffe44a544
Hospuntake <91428070+Hospuntake@users.noreply.github.com>
2021-11-30 18:11:57 +0100

    definitivo entrega

---
+++ 4replicant.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "dummy4.h"
+
+int main(int argc,char *argv[]){
+	int Memoria = atoi(argv[1]);
+    char* buffer=malloc(Memoria);
+    dummy_init(buffer,Memoria);
+    
+    int by;
+    while((by=read(0,buffer,Memoria))>0){
+        dummy_calc(buffer, by);
+    }
+    dummy_end();
+    exit(0);//no entra
+
+
+
+}
